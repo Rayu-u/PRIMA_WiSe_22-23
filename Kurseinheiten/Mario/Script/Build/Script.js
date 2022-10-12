2f8fdea7f182968703e055f46cd7014c68a5bfc4
Laura Fabricius <62756038+Rayu-u@users.noreply.github.com>
2022-10-12 16:00:41 +0200

    Mario Runs, but right now just on window load

--- Kurseinheiten/Mario/Script/Build/Script.js
+++ Kurseinheiten/Mario/Script/Build/Script.js
@@ -1,1 +1,110 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    // Add eventlistener for the loading of the window
+    window.addEventListener("load", onLoad);
+    // Define marioSpriteNode from FUDGE
+    let marioSpriteNode;
+    // load Handler
+    async function onLoad(_event) {
+        // make new Node with name root
+        let root = new ƒ.Node("root");
+        // load spritesheet from folder and add a "coat" to it.
+        let marioSpriteSheet = new ƒ.TextureImage();
+        await marioSpriteSheet.load("Spritesheets/Mario/Mario_final-Sheet.png");
+        let coat = new ƒ.CoatTextured(undefined, marioSpriteSheet);
+        // add running animation
+        let marioAnimation = new ƒAid.SpriteSheetAnimation("Mario_Run", coat);
+        marioAnimation.generateByGrid(ƒ.Rectangle.GET(0, 56, 40, 56), 12, 40, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(40));
+        marioSpriteNode = new ƒAid.NodeSprite("Mario_Sprite");
+        // adds a transform component to the sprite
+        marioSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        marioSpriteNode.setAnimation(marioAnimation);
+        // play animation forwards
+        marioSpriteNode.setFrameDirection(1);
+        // wohl unnötig?
+        marioSpriteNode.mtxLocal.translateY(-1);
+        //set framerate here
+        marioSpriteNode.framerate = 10;
+        root.addChild(marioSpriteNode);
+        // camera setup
+        let cmpCamera = new ƒ.ComponentCamera();
+        cmpCamera.mtxPivot.translateZ(5);
+        cmpCamera.mtxPivot.rotateY(180);
+        // setup viewport
+        const canvas = document.querySelector("canvas");
+        viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", root, cmpCamera, canvas);
+        viewport.camera.clrBackground = ƒ.Color.CSS("White");
+        viewport.draw();
+        // actually let the loop run
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, loop);
+        // edit framerate here
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
+        // was ist change?
+        document.forms[0].addEventListener("change", onChange);
+    }
+    function loop(_event) {
+        // is this even useful anymore now?
+        viewport.draw();
+    }
+    function onChange(_event) {
+        // framerate is set here again? How do these connect?
+        let value = parseInt(_event.target.value);
+        marioSpriteNode.framerate = value;
+        console.log("framerate set to: " + value);
+    }
+    document.addEventListener("interactiveViewportStarted", start);
+    function start(_event) {
+        viewport = _event.detail;
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        // ƒ.Physics.simulate();  // if physics is included and used
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Kurseinheiten/Mario/Script/Source/Main.ts
+++ Kurseinheiten/Mario/Script/Source/Main.ts
@@ -1,8 +1,81 @@
 namespace Script {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+
+  // Add eventlistener for the loading of the window
+  window.addEventListener("load", onLoad);
+  // Define marioSpriteNode from FUDGE
+  let marioSpriteNode: ƒAid.NodeSprite;
+
+  // load Handler
+  async function onLoad(_event: Event): Promise<void> {
+    // make new Node with name root
+    let root: ƒ.Node = new ƒ.Node("root");
+
+    // load spritesheet from folder and add a "coat" to it.
+    let marioSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await marioSpriteSheet.load("Spritesheets/Mario/Mario_final-Sheet.png");
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, marioSpriteSheet);
+    
+    // add running animation
+    let marioAnimation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("Mario_Run", coat);
+    marioAnimation.generateByGrid(ƒ.Rectangle.GET(0, 56, 40, 56), 12, 40, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(40));
+    
+
+    marioSpriteNode = new ƒAid.NodeSprite("Mario_Sprite");
+
+    // adds a transform component to the sprite
+    marioSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+    marioSpriteNode.setAnimation(marioAnimation);
+    // play animation forwards
+    marioSpriteNode.setFrameDirection(1);
+
+    // wohl unnötig?
+    marioSpriteNode.mtxLocal.translateY(-1);
+
+    //set framerate here
+    marioSpriteNode.framerate = 10;
+
+    root.addChild(marioSpriteNode);
+
+    // camera setup
+    let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+    cmpCamera.mtxPivot.translateZ(5);
+    cmpCamera.mtxPivot.rotateY(180);
+
+    // setup viewport
+    const canvas: HTMLCanvasElement = document.querySelector("canvas");
+
+    viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", root, cmpCamera, canvas);
+    viewport.camera.clrBackground = ƒ.Color.CSS("White");
+    viewport.draw();
+
+    // actually let the loop run
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, loop);
+    // edit framerate here
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
+
+    // was ist change?
+    document.forms[0].addEventListener("change", onChange);
+  }
+
+  function loop(_event: Event): void {
+
+    // is this even useful anymore now?
+    viewport.draw();
+  }
+
+  function onChange(_event: Event): void {
+    // framerate is set here again? How do these connect?
+    let value: number = parseInt((<HTMLInputElement>_event.target).value);
+    marioSpriteNode.framerate = value;
+    console.log("framerate set to: " + value);
+  }
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
@@ -17,4 +90,6 @@ namespace Script {
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
+
+
 }
