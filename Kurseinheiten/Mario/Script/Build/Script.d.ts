dfe9787cd5293f2b4c50a7bfcfc905b8b78b0071
Laura Fabricius <62756038+Rayu-u@users.noreply.github.com>
2022-11-21 23:57:29 +0100

    clean up

--- Kurseinheiten/Mario/Script/Build/Script.d.ts
+++ Kurseinheiten/Mario/Script/Build/Script.d.ts
@@ -24,23 +24,6 @@ declare namespace Script {
     }
 }
 declare namespace Script {
-    enum Direction {
-        LEFT = 0,
-        RIGHT = 1
-    }
-}
-declare namespace Script {
-}
-declare namespace Script {
-    import ƒ = FudgeCore;
-    class MarioScript extends ƒ.ComponentScript {
-        static readonly iSubclass: number;
-        message: string;
-        constructor();
-        private onInput;
-        private update;
-        hndEvent: (_event: Event) => void;
-    }
 }
 declare namespace Script {
     enum MarioState {
@@ -60,65 +43,6 @@ declare namespace Script {
         changeState(nextState: String): void;
         setOrientation(orientation: string): void;
     }
-}
-declare namespace Script {
-    interface State {
-        update(inputState: InputState): State;
-    }
-    class IdleState implements State {
-        update(inputState: InputState): State;
-    }
-    class WalkState implements State {
-        update(inputState: InputState): State;
-    }
-    class RunState implements State {
-        update(inputState: InputState): State;
-    }
-    class JumpState implements State {
-        update(inputState: InputState): State;
-    }
-    class DeathState implements State {
-        update(inputState: InputState): State;
-    }
-    let idleState: IdleState;
-    let walkState: WalkState;
-    let runState: RunState;
-    let jumpState: JumpState;
-    let deathState: JumpState;
-}
-declare namespace Script {
-    import ƒAid = FudgeAid;
-    enum JOB {
-        IDLE = 0,
-        ESCAPE = 1,
-        DIE = 2,
-        RESPAWN = 3
-    }
-    export class StateMachine extends ƒAid.ComponentStateMachine<JOB> {
-    }
-    export {};
-}
-declare namespace Script {
-    import ƒAid = FudgeAid;
-    enum MarioStates {
-        IDLE = 0,
-        WALK = 1,
-        RUN = 2
-    }
-    export class StateMachineMario extends ƒAid.ComponentStateMachine<MarioStates> {
-        static readonly iSubclass: number;
-        private static instructions;
-        constructor();
-        static get(): ƒAid.StateMachineInstructions<MarioStates>;
-        private static actIdle;
-        private static actWalk;
-        private static actRun;
-        private static transitDefault;
-        private static actDefault;
-        private hndEvent;
-        private hndKeyInput;
-    }
-    export {};
 }
 declare namespace Script {
     interface InputState {
